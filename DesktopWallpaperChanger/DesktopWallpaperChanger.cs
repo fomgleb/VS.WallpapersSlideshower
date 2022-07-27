0d6f30baad75153cb7e7f6d73e7ac2cb38bb0ba3
Gleb Fomenko <fomglib@gmail.com>
2022-07-27 21:22:38 +0300

    Upgrade wallpapers changer

--- DesktopWallpaperChanger/DesktopWallpaperChanger.cs
+++ DesktopWallpaperChanger/DesktopWallpaperChanger.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 namespace DesktopWallpaper
 {
@@ -18,42 +19,69 @@ namespace DesktopWallpaper
 
         public enum Style : int
         {
-            Tiled,
-            Centered,
-            Stretched
+            Fill,
+            Fit,
+            Span,
+            Stretch,
+            Tile,
+            Center
         }
 
-        public static void Set(Uri uri, Style style)
+        private static Task _currentSetWallpaperTask;
+        public static async void SetWallpaperAsync(Uri uri, Style style)
         {
-            Stream s = new System.Net.WebClient().OpenRead(uri.ToString());
-
-            System.Drawing.Image img = System.Drawing.Image.FromStream(s);
-            string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
-            img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
-
-            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
-            if (style == Style.Stretched)
+            if (_currentSetWallpaperTask != null)
             {
-                key.SetValue(@"WallpaperStyle", 2.ToString());
-                key.SetValue(@"TileWallpaper", 0.ToString());
+                await _currentSetWallpaperTask;
             }
 
-            if (style == Style.Centered)
+            _currentSetWallpaperTask = Task.Run(() =>
             {
-                key.SetValue(@"WallpaperStyle", 1.ToString());
-                key.SetValue(@"TileWallpaper", 0.ToString());
-            }
+                Stream s = new System.Net.WebClient().OpenRead(uri.ToString());
 
-            if (style == Style.Tiled)
-            {
-                key.SetValue(@"WallpaperStyle", 1.ToString());
-                key.SetValue(@"TileWallpaper", 1.ToString());
-            }
+                System.Drawing.Image img = System.Drawing.Image.FromStream(s);
+                string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+                img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
 
-            SystemParametersInfo(SPI_SETDESKWALLPAPER,
-                0,
-                tempPath,
-                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+                if (style == Style.Fill)
+                {
+                    key.SetValue(@"WallpaperStyle", 10.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                }
+                if (style == Style.Fit)
+                {
+                    key.SetValue(@"WallpaperStyle", 6.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                }
+                if (style == Style.Span) // Windows 8 or newer only!
+                {
+                    key.SetValue(@"WallpaperStyle", 22.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                }
+                if (style == Style.Stretch)
+                {
+                    key.SetValue(@"WallpaperStyle", 2.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                }
+                if (style == Style.Tile)
+                {
+                    key.SetValue(@"WallpaperStyle", 0.ToString());
+                    key.SetValue(@"TileWallpaper", 1.ToString());
+                }
+                if (style == Style.Center)
+                {
+                    key.SetValue(@"WallpaperStyle", 0.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                }
+
+                SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                    0,
+                    tempPath,
+                    SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+            });
+
+            await _currentSetWallpaperTask;
         }
     }
 }
