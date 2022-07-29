178f1fae52240188ed43642af94fd8d09a788fbc
Gleb Fomenko <fomglib@gmail.com>
2022-07-29 13:37:40 +0300

    Rename WallpapersSlideshower to WallpapersSlideshowerFramework. Move logic from DesktopWallpaperChanger to WallpapersslideshowerFramework

--- WallpapersSlideshowerFramework/Models/DesktopWallpaperChanger.cs
+++ WallpapersSlideshowerFramework/Models/DesktopWallpaperChanger.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
-namespace DesktopWallpaper
+namespace WallpapersSlideshower
 {
     public sealed class DesktopWallpaperChanger
     {
